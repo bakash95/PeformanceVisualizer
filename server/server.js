f6bedce29dbaa033dcbacab2546dcfa28ad5bab3
Akash <akash@akash.com>
2020-04-18 21:57:14 +0530

    heroku deploy fix

--- server/server.js
+++ server/server.js
@@ -1,28 +1,12 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let fs = require('fs');
-let path = require('path');
-
-let app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-app.post('/getPerformanceData',
-    (req, res) => {
-        let menuPath = path.join(__dirname, '/performanceData.json');
-        let menuPathFile = fs.readFileSync(menuPath);
-        res.setHeader('Content-Type','application/json');
-        setTimeout(()=>{
-            res.write(menuPathFile)
-            res.end()
-        },2000)
-    })
-
-
-app.listen('3003');
+app.listen(port, () => {
+   console.log('Server is up!');
+});
