e9aa94cf3095d6bb987a770435a0f6fecb04c73e
Akash <akash@akash.com>
2020-04-20 00:24:09 +0530

    locale for malay

--- src/dictionary/dictionary.js
+++ src/dictionary/dictionary.js
@@ -16,13 +16,22 @@ let dictionaryEn = {
     'static2_text_chart':'Gross returns and exchanges rates sources from ',
 }
 
-let dictionaryChi = {
-    'Home': 'Home1',
-    'Manage_Deposits': 'Manage Deposits',
-    'Refer_friend': 'Refer a friend',
-    'Support': 'Support',
-    'General_Investing': 'General Investing',
-    'overview':'overview',
+let dictionaryMalay = {
+    'Home': 'Rumah',
+    'Manage_Deposits': 'Urus Deposit',
+    'Refer_friend': 'Rujuk rakan',
+    'Support': 'Sokongan',
+    'General_Investing': 'Pelaburan Umum',
+    'overview':'gambaran keseluruhan',
+    'portfolio_benchmark': 'Penanda aras portfolio',
+    'stashaway_index_name': 'Indeks Risiko Stash Away 14%',
+    'benchmark_placeholder': 'Tanda aras mana yang anda mahu bandingkan',
+    'assets': 'Aset',
+    'projection': 'Unjuran',
+    'about_portfolio': 'Mengenai Portfolio',
+    'disclaimer_mobile': 'Silakan Miringkan telefon anda untuk pengalaman terbaik',
+    'static1_text_chart': 'Nilai PortFolio berdasarkan pulangan kasar',
+    'static2_text_chart': 'Pulangan kasar dan pertukaran sumber kadar dari',
 }
 
-export {dictionaryEn as en,dictionaryChi as ch};
+export {dictionaryEn as en,dictionaryMalay as my};
